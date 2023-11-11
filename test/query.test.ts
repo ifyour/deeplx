d97f3db9f1a7d2d24127012f959f9643b954e293
ifyour <ifyour@outlook.com>
2023-11-11 11:16:39 +0800

    feat: unit test coverage

--- test/query.test.ts
+++ test/query.test.ts
@@ -1,47 +1,80 @@
+import fetch from 'jest-fetch-mock';
 import { query } from '../src/query';
-import { API_URL } from '../src/const';
+import { ResponseParams } from '../src/types';
 
-// @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () =>
-      Promise.resolve({
-        id: 1,
-        result: {
-          texts: [
-            {
-              text: 'Translated text',
-              alternatives: [{ text: 'Translated text' }],
-            },
-          ],
-        },
-      }),
-  })
-);
+jest.setMock('node-fetch', fetch);
 
 describe('query', () => {
-  it('should have a correct return format', async () => {
-    const response = await query({
-      text: '翻译文本',
-      source_lang: 'zh',
-      target_lang: 'en',
-    });
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(
-      API_URL,
-      expect.objectContaining({
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      })
-    );
-    expect(response).toEqual({
-      code: 200,
-      message: 'success',
-      data: 'Translated text',
-      alternatives: ['Translated text'],
-      source_lang: 'zh',
-      target_lang: 'en',
-    });
+  it('should respond with 404', async () => {
+    const result = {
+      code: 404,
+      message: 'No Translate Text Found',
+      data: null,
+    };
+    // @ts-ignore
+    const response1 = await query();
+    expect(response1).toEqual(result);
+
+    // @ts-ignore
+    const response2 = await query({});
+    expect(response2).toEqual(result);
+
+    // @ts-ignore
+    const response3 = await query({ text: '' });
+    expect(response3).toEqual(result);
+  });
+
+  it('should be a correct return result (zh -> en)', async () => {
+    let response: ResponseParams | undefined;
+    try {
+      response = await query({
+        text: '翻译文本',
+        source_lang: 'zh',
+        target_lang: 'en',
+      });
+      expect(response.code).toEqual(200);
+      expect(response.message).toEqual('success');
+      expect(response.data).toEqual('Translated text');
+      expect(response.source_lang).toEqual('zh');
+      expect(response.target_lang).toEqual('en');
+      expect(response.alternatives).toBeInstanceOf(Array);
+      if (!!response.alternatives?.length) {
+        expect(
+          response.alternatives.every(
+            (text: string) => typeof text === 'string'
+          )
+        ).toBe(true);
+      }
+    } catch (error) {
+      console.log('>> response', response);
+      throw error;
+    }
+  });
+
+  it('should be a correct return result (en -> zh)', async () => {
+    let response: ResponseParams | undefined;
+    try {
+      response = await query({
+        text: 'Translated text',
+        source_lang: 'en',
+        target_lang: 'zh',
+      });
+      expect(response.code).toEqual(200);
+      expect(response.message).toEqual('success');
+      expect(response.data).toEqual('翻译文本');
+      expect(response.source_lang).toEqual('en');
+      expect(response.target_lang).toEqual('zh');
+      expect(response.alternatives).toBeInstanceOf(Array);
+      if (!!response.alternatives?.length) {
+        expect(
+          response.alternatives.every(
+            (text: string) => typeof text === 'string'
+          )
+        ).toBe(true);
+      }
+    } catch (error) {
+      console.log('>> response', response);
+      throw error;
+    }
   });
 });
